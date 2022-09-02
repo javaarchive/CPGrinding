d1527ad0cf534c7edfe5cacf924c40c5442e96f9
Raymond <raymond.peng07@gmail.com>
2022-09-02 06:03:14 +0000

    ext extract script

---
+++ extensions.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+echo Downloading external extensions
+wget "https://drive.google.com/uc?id=1pMWDYJ-VXTiH2pz2r2FSNu7Q9kz32omW&confirm=t&uuid=0a732da0-456d-4c99-805f-72e9536377f8" -O devpack.zip
+unzip devpack.zip
