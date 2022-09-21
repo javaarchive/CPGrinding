ac16bc68d8a6253cec2b92355c23a01b86a9f923
Raymond <raymond.peng07@gmail.com>
2022-09-21 04:12:32 +0000

    two pointer / sliding E

--- E_Binary_Deque.cpp
+++ E_Binary_Deque.cpp
@@ -1,78 +1,56 @@
 #include <bits/stdc++.h>
+#define MAXN 200003
 using namespace std;
-#define MAXN 200005
 
-int N, S;
 int arr[MAXN];
-int ps[MAXN];
-int query(int start, int end){
-    return ps[end + 1] - ps[start];
-}
+int N, S;
+
 void solve(){
     cin >> N >> S;
+    
     for(int i = 0; i < N; i ++){
-        cin >> arr[i];
-        ps[i + 1] = 0;
+        cin >> arr[i];    
     }
-    // cout << "OK!" << endl;
-    ps[0] = 0;
-    // build ps
-    for(int j = 0; j < N; j ++){
-        ps[j + 1] = ps[j] + arr[j];
-    }
-
-    // cout << "OK " << endl;
-    /*
-    function binary_search(A, n, T) is
-    L := 0
-    R := n − 1
-    while L ≤ R do
-        m := floor((L + R) / 2)
-        if A[m] < T then
-            L := m + 1
-        else if A[m] > T then
-            R := m − 1
-        else:
-            return m
-    return unsuccessful
-    */
-   int best = INT32_MAX;
-   int loops = 0;
-   for(int s = 0; s < N; s ++){
-        int L = s;
-        int R = N - 1;
-       
-        while(L <= R){
-            loops ++;
-            int e = floor((L + R) / 2);
-            // cout << L << " " << e << " " << R << endl;
-            int v = query(s,e);
-            if(loops > 1000){
-                cout << L << " " << e << " " << R << endl;
-                cout << "V: " << v << " s " << s<< " e " << e << endl;
-                break;
-            }
-            if(v < S){
-                L = v + 1;
-            }else if(v > S){
-                R = v - 1;
-            }else{
-                int cost = (s-1) + ((N - 1) - (v + 1) + 1);
-                best = min(best, cost);
-                break;
-            }
+    // Two pointers / Sliding window
+    int sum = 0;
+    int ending = 0;
+    int best = INT32_MAX;
+    for(int start = 0; start < N; start ++){
+        if(start > 0){
+            /*if(arr[start - 1] > 0){
+                cout << "- " << arr[start - 1] << endl;
+            }*/
+            sum -= arr[start - 1];
         }
-        // no change ig
-   }
-   if(best = INT32_MAX) best = -1;
-   cout << best << endl;
+        ending = max(ending, start);
+        bool looped = false;
+        for(; ending < N; ending ++){
+            if(sum == S && arr[ending] != 0){
+                break;
+            }
+            looped = true;
+            // cout << "+ arr[" << ending << "] = " << arr[ending] << endl; 
+            sum += arr[ending];
+        }
+        // if(looped) ending --; // last good
+        // cout << start << " < --- > " << ending-1 << " sum: " << sum << endl;
+        if(sum != S){
+            // not enough
+            break;
+        }
+        // sum == S
+        int cost = start + N - 1 - (ending-1) ;
+        // cout << "Final cost " << cost << endl;
+        best = min(best, cost);
+    }
+    // cout << "ANS: ";
+    cout << (best == INT32_MAX ? -1 : best) << endl;
 }
 
 int main(int argc, char const *argv[])
 {
-    int T;
-    cin >> T;
-    // cout << "AGH" << endl;
-    while(T --) solve();    
+    int NN;
+    cin >> NN;
+    while(NN --) solve();
     return 0;
 }
