96cbb15dca8c6248ad4ce1f11411b4810f39cbc9
Raymomyar <awesomecatstudio@gmail.com>
2023-02-19 11:36:55 -0800

    scaf

--- D_Walking_Between_Houses.cpp
+++ D_Walking_Between_Houses.cpp
@@ -1,7 +1,26 @@
 #include <bits/stdc++.h>
+using namespace std;
+
+int N, K, S;
+// K moves
+// S distance
 
 void solve(){
-    
+    cin >> N >> K >> S;
+    int remainingMoves = K;
+    int remainingDist = S;
+    int rDelta;
+    int curPos = 1;
+    while(remainingMoves > 0){
+        rDelta = remainingDist - remainingMoves;
+        // We want to spam a jump
+        int jumpA = curPos - 1;
+        int jumpB = N - curPos;
+        int bestJump = max(jumpA, jumpB);
+        if(bestJump > rDelta){
+            
+        }
+    }
 }
 
 int main(int argc, char const *argv[])
