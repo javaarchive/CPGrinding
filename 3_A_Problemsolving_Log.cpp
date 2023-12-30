f5260ad38ec29124ed32bb36ccd7c64248fda9cb
Raymond <20248577+javaarchive@users.noreply.github.com>
2023-12-29 23:29:59 -0800

    very bad header hack + solve 3A

--- 3_A_Problemsolving_Log.cpp
+++ 3_A_Problemsolving_Log.cpp
@@ -1,14 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
+int N;
+string line;
 
 void solve(){
-    
+    map<char,int> freq;
+    cin >> N;
+    cin >> line;
+    for(int i = 0; i < N; i ++){
+        freq[line[i]] ++;
+    }
+    int ans = 0;
+    for(char c = 'A'; c <= 'Z'; c ++){
+        int requirement = ((int) c ) - ((int) 'A') + 1;
+        if (freq[c] >= requirement) {
+            ans ++;
+        }
+    }
+    cout << ans << endl;
 }
 
 int main(int argc, char const *argv[])
 {
-    solve();
+    int T;
+    cin >> T;
+    while(T --) solve();
     return 0;
 }
